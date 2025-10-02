c59f44814a0b4b9eb18efcf7ba07012688771f5f
prianto dey <priantodey12@gmail.com>
2025-10-02 23:29:07 +0600

    Added ProductTwoNumbers, DivTwoNum, Arithmetic, TwoNumSum solutions

---
+++ java-basics/DivTwoNum.java
@@ -0,0 +1,5 @@
+public class DivTwoNum {
+    public static void main(String[] args) {
+        System.out.println("Division of 10 and 2 is : "+(10/2));
+    }
+}
---
+++ java-basics/TwoNumSum.java
@@ -0,0 +1,5 @@
+public class TwoNumSum {
+    public static void main(String[] args) {
+        System.out.println("Sum of 1 and 2 is :"+(1+2));
+    }
+}
