6040fdfa75ee59d82b3771d261726b49f7cb8d76
prianto dey <priantodey12@gmail.com>
2025-10-10 12:10:53 +0600

    count divisible problem

---
+++ java-basics/CountDivisiblesRange.java
@@ -0,0 +1,23 @@
+
+/*
+ 56. Count Divisibles in Range
+
+Write a Java program to find the number of values in a given range divisible by a given value.
+For example x = 5, y=20 and p =3, find the number of integers within the range x..y and that are divisible by p i.e. { i :x ≤ i ≤ y, i mod p = 0 }
+Sample Output:
+
+5
+ */
+public class CountDivisiblesRange {
+    public static void main(String[] args) {
+        int count = 0;
+
+        for(int i=5;i<=20;i++) {
+            if(i%3==0) {
+                count++;
+            }
+        }
+        System.out.print("Sample output : "+count);
+    }
+    
+}
