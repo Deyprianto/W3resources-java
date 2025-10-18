fdf1849b360894f0772ee1ad4d7d31b825d4cd42
prianto dey <priantodey12@gmail.com>
2025-10-18 11:04:57 +0600

    system environment and property chk

---
+++ java-basics/EnvironmentProperty.java
@@ -0,0 +1,12 @@
+
+/*
+88. System Environment and Properties
+
+Write a Java program to get the current system environment and system properties.
+ */
+public class EnvironmentProperty {
+    public static void main(String[] args) {
+        System.out.println(System.getenv());
+        System.out.println(System.getProperties());
+    }
+}
