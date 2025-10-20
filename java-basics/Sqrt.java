a1d267d99909196baa60ae3c10366bb3a3bd2331
prianto dey <priantodey12@gmail.com>
2025-10-20 13:09:03 +0600

    sqrt problem

---
+++ java-basics/Sqrt.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class Sqrt {
+    public static void main(String[] args) {
+        Scanner input  =new Scanner(System.in);
+        int n = input.nextInt();
+        int x  = (int)Math.sqrt(n);
+        System.out.println(x);
+    }
+}
