b24ac2f13987e005e804b46f4bda05a8d63a2fe2
prianto dey <priantodey12@gmail.com>
2025-10-23 16:34:51 +0600

    expression evaluation

--- java-basics2/EvaluateExpressios.java
+++ java-basics2/EvaluateExpressios.java
@@ -1,3 +1,24 @@
+/*
+ * 151. Evaluate Expressions
+
+Write a Java program to find the value of a specified expression.
+
+a) 101 + 0) / 3
+b) 3.0e-6 * 10000000.1
+c) true && true
+d) false && true
+e) (false && false) || (true && true)
+f) (false || false) && (true && true)
+
+Expected Output :
+(101 + 0) / 3)-> 33
+(3.0e-6 * 10000000.1)-> 30.0000003
+(true && true)-> true
+(false && true)-> false
+((false && false) || (true && true))-> true
+(false || false) && (true && true)-> false
+ */
+
 class EvaluateExpressions {
    public static void main(String[] args) {
     System.out.println("(101+0)/3 :"+(101+0)/3);
