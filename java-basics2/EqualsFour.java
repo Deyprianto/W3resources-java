2ae557a0af23e803fbf67739b245a683d4d9d6c9
prianto dey <priantodey12@gmail.com>
2025-10-23 17:51:17 +0600

    equals or not chk

---
+++ java-basics2/EqualsFour.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+
+/*
+ * 152. Check Four Numbers Equal
+
+Write a Java program that accepts four integers from the user and prints equal if all four are equal, and not equal otherwise.
+
+Sample Output:
+Input first number: 25
+Input second number: 37
+Input third number: 45
+Input fourth number: 23
+Numbers are not equal!
+ */
+public class EqualsFour {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        if(a==b && b==c && c==d) {
+            System.out.println("Numbers are equals");
+        } else {
+            System.out.println("Numbers are not equals");
+        }
+    }
+    
+}
